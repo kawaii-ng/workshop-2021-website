33f72846c02d8e928339d9552ec22e81ea25c8b3
Kawaii Ng <nkw721@gmail.com>
2022-01-08 16:09:40 +0800

    add tutorial

--- src/pages/SoftwarePage/SoftwarePage.js
+++ src/pages/SoftwarePage/SoftwarePage.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import softwareIcon from '../../images/codelab-adaptar.PNG'
 import { Scrollbars } from 'react-custom-scrollbars'
+import tutVideo from '../../videos/install_method.mp4';
 
 function SoftwarePage() {
     return (
         <Scrollbars style={{height: '100%', width: '100%'}}>
-            <h1>Codelab Adaptar</h1>
-            <img src={softwareIcon} alt="" style={{margin: '1em', borderRadius: 25}}/>
+            {/* <h1>Codelab Adaptar</h1>
+            <img src={softwareIcon} alt="" style={{margin: '1em', borderRadius: 25}}/> */}
             <h1>下載及安裝Codelab Adaptar</h1>
+            <video width="320" height="240" controls>
+                <source src={tutVideo} type="video/mp4"></source>
+                Your browser does not support the video tag.
+            </video>
             <div><b>官方網站： </b><a href="https://adapter.codelab.club/get_start/gs_install/?version=latest" target="_blank">https://adapter.codelab.club/get_start/gs_install/?version=latest</a></div>
             <div><b>安裝教學： </b><a href="https://adapter.codelab.club/get_start/gs_install/?version=latest#win" target="_blank">立即前往</a></div>
             <div><b>下載連結： </b><a href="https://scratch3-files.just4fun.site/codelab-adapter-4_9_6-win64.zip" target="_blank" download="https://scratch3-files.just4fun.site/codelab-adapter-4_9_6-win64.zip">立即下載</a></div>
